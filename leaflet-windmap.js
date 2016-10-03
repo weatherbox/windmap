fd77581eb83c55bb95a218a13473cb0fd122895c
tattii <yuta.tatti@gmail.com>
2016-10-04 02:19:55 +0900

    show point wind

--- leaflet-windmap.js
+++ leaflet-windmap.js
@@ -29,6 +29,9 @@ L.Windmap = L.Class.extend({
 			url = url.replace("{level}", "surface");
 			self._initStreamline(url);	
 		});
+
+		// set click event
+		map.on("click", this.showPointWind, this);
 	},
 
 	_initStreamline: function (url){
@@ -51,10 +54,37 @@ L.Windmap = L.Class.extend({
 	// loading animation by sonic.js
 	createLoading: function () {
 		var sonic = new Sonic(SonicLoader);
-		this._loading = $("#loading");
-		this._loading.append(sonic.canvas);
+		var loading = $("#loading");
+		loading.append(sonic.canvas);
 		sonic.play();
 	},
+
+	showPointWind: function (e) {
+		var latlng = e.latlng;
+		var v = this._grib2tile.getVector(latlng);
+		if (v[0] != null){
+			var speed = Math.sqrt(v[0]*v[0] + v[1]*v[1]);
+			var ang = Math.acos(v[1]/speed) / Math.PI * 180;
+			if (v[0] < 0) ang = 360 - ang;
+			$("#wind-dialog").text(Math.round(ang) + "° "  +Math.round(speed*10)/10 + "m/s");
+			if (this._pointMarker) {
+				this._pointMarker.setLatLng(latlng);
+			}else{
+				var circleIcon = L.divIcon({
+					iconSize: [20, 20],
+					iconAnchor: [10, 10],
+					className: "",
+					html: '<svg width="20" height="20"><circle cx="10" cy="10" r="6" fill="none"   stroke="#3aff3a" stroke-width="2.5"/></svg>'
+				});
+				this._pointMarker = L.marker(latlng, {icon:circleIcon}).addTo(this._map);
+			}
+		}
+	},
 	
+	hidePointWind: function() {
+		if (this._pointMarker) this._map.removeLayer(this._pointMarker);
+		this._pointMarker = null;
+		$("#wind-dialog").text("");
+	}
 });
 
