ca7dd683f5a309f001d7c3143ce3b48bcef06ae9
tattii <yuta.tatti@gmail.com>
2016-12-18 04:12:53 +0900

    generate days

--- components/TimeSliderPC.js
+++ components/TimeSliderPC.js
@@ -35,10 +35,48 @@ const styles = {
 	},
 }
 
+const HOUR = 3600 * 1000
+
 
 export default class TimeSliderPC extends React.Component {
 	state = {
 		hover: true,
+	}
+
+	constructor(props) {
+		super(props)
+
+		this.time = this.props.now
+		this.percent = (this.time - this.props.start) / (this.props.end - this.props.start) * 100
+
+		this.days = []
+		this._createDayList()
+	}
+
+	_createDayList = () => {
+		let startDate = new Date(this.props.start)
+		let hours = 24 - startDate.getHours()
+		this.days.push({
+			hours: hours,
+			day: this._getDayString(startDate)
+		})
+
+		for (var d = this.props.start + hours * HOUR; d < this.props.end - 24 * HOUR; d += 24 * HOUR){
+			this.days.push({
+				hours: 24,
+				day: this._getDayString(new Date(d))
+			})
+		}
+
+		let endDate = new Date(this.props.end)
+		this.days.push({
+			hours: endDate.getHours(),
+			day: this._getDayString(endDate)
+		})
+	}
+
+	_getDayString = (date) => {
+		return (date.getMonth() + 1) + '/' + ('0' + date.getDate()).slice(-2)
 	}
 
 	click = (e) => {
@@ -50,7 +88,39 @@ export default class TimeSliderPC extends React.Component {
 	}
 
 	render() {
-		let self = this
+		let days = this.days
+		this.width = 6 * ((this.props.end - this.props.start) / HOUR)
+
+		let progress = (
+			<div style={ styles.progressDiv }>
+				<Progress
+					size='tiny'
+					color='blue'
+					className='time-slider-progress'
+					percent={this.percent}
+					onClick={this.click}
+					onMouseOver={this.hover}
+					onMouseMove={this.hover}
+					style={{ width: this.width, marginBottom: 4 }} />
+
+				<div style={ styles.days }>
+					{days.map((d, i) => {
+						let style = Object.assign({}, styles.day)
+						let last = (i == days.length - 1)
+						let width = 6 * d.hours
+						if (!last) width -= 1
+						style.width = 6 * d.hours - last
+						if (last) style.borderRight = 'none'
+						let day = (d.hours > 3) ? d.day : ''
+
+						return (
+							<div style={style} key={i}>{day}</div>
+						)
+					})}
+				</div>
+
+			</div>
+		)
 
 		return (
 			<div style={{ display:'inline-block', verticalAlign:'bottom' }}>
@@ -60,34 +130,11 @@ export default class TimeSliderPC extends React.Component {
 					direction='bottom'
 					className='time-slider-pc'
 					visible={this.props.visible}>
-					{(() => {
-						if (self.props.visible){
-							return (
-								<div style={ styles.progressDiv }>
-									<Progress
-										size='tiny'
-										color='blue'
-										className='time-slider-progress'
-										percent={50}
-										onClick={self.click}
-										onMouseOver={self.hover}
-										onMouseMove={self.hover}
-										style={{ width: 306, marginBottom: 4 }} />
-
-									<div style={ styles.days }>
-										<div style={Object.assign({}, styles.day, { width:72 })}>12/17</div>
-										<div style={Object.assign({}, styles.day, {})}>12/18</div>
-										<div style={Object.assign({}, styles.day, { width:90, border:'none' })}>12/19</div>
-									</div>
-
-								</div>
-							)
-						}
-					})()}
+					{ this.props.visible && progress }
 				</Sidebar>
 
-				<div className={'ui popup transition top center ' + ((self.props.visible && self.state.hover) ? "visible" : "")} style={styles.popup}>
-					{self.props.time}
+				<div className={'ui popup transition top center ' + ((this.props.visible && this.state.hover) ? "visible" : "")} style={styles.popup}>
+					{this.props.time}
 				</div>
 			</div>
 		)
